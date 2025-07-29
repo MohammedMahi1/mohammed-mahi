8fd58e4eb350ed5a22f986811a3b2684522f4450
Mohammed Mahi <mohammed.mahi012@gmail.com>
2025-07-30 00:22:38 +0100

    edit : reach me page

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,30 +1,26 @@
-
 type NavbarItem = {
   label: string;
-  id:number;
+  id: number;
   path: string;
-}
+};
 
 type NavbarProps = {
-    items:NavbarItem[];
-}
+  items: NavbarItem[];
+};
 
-const Navbar = ({items}:NavbarProps) => {
+const Navbar = ({ items }: NavbarProps) => {
   return (
-    <div className='navbar'>
-        {items.map((e) => (
-            <>
-                        <a key={e.id} href={e.path} className='link'>
-                {e.label}
-            </a>
-            {
-                e.id !== items.length && <span className='divider'>&</span>
-            }
-            </>
-            ))
-            }
+    <div className="navbar">
+      {items.map((e) => (
+        <>
+          <a key={e.id} href={e.path} className="link">
+            {e.label}
+          </a>
+          {e.id !== items.length && <span className="divider">&</span>}
+        </>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/ReachMe.tsx
+++ src/pages/ReachMe.tsx
@@ -1,8 +1,17 @@
-import { AtSign } from "lucide-react";
+import { Check, Copy } from "lucide-react";
+import Button from "../components/ui/button";
 import Label from "../components/ui/label";
-import { Link } from "react-router";
+import { useState } from "react";
 
 const ReachMe = () => {
+    const [isCopied,setIsCopied] = useState(false)
+  const handleCopy = (event: any) => {
+    navigator.clipboard.writeText(event.target.innerText);
+    setIsCopied(true);
+    setTimeout(()=>{
+        setIsCopied(false)
+    },1000)
+  };
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
       <Label>📬 Reach Me</Label>
@@ -12,14 +21,20 @@ const ReachMe = () => {
         Feel free to reach out — I’d love to hear from you!
       </Label>
       <Label>👉 Click the button below to copy my email:</Label>
-      <Link to="mailto:mohammed.mahi012@gmail.com?body=My custom mail body">
-
-        
-            <AtSign/>
-        
+      <Button
+        onClick={(e: any) => {
+          handleCopy(e);
+        }}
+      >
+        {
+            isCopied ?
+            
+            <Check/>
+            :
+            <Copy />
+        }
         <span>mohammed.mahi012@gmail.com</span>
-      </Link>
-      
+      </Button>
     </div>
   );
 };
