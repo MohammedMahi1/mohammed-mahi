060d39f892c6f5c59cc952a805b8cf823df7eab3
Mohammed Mahi <mohammed.mahi012@gmail.com>
2025-07-29 21:06:27 +0100

    style: enhance layout and styling of Me page, update icon group and label sizes

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -1,18 +1,37 @@
+import {
+  Github,
+  Instagram,
+  Linkedin,
+  LinkedinIcon,
+  LucideLinkedin,
+  Twitter,
+  X,
+} from "lucide-react";
 import Label from "../components/ui/label";
 
 const Me = () => {
   return (
     <div className="content">
-      <Label>
-        Hello 👋
-        <br />
-        I'm Mohammed Mahi 🧑‍💻
-      </Label>
-      <Label>Full Stack Web Developer | Laravel & React Specialist</Label>
-      <Label>
-        I craft modern, responsive, and scalable web applications using powerful tools like Laravel, React.js, and Tailwind CSS.
-         I focus on building clean UI, secure backend systems, and smooth user experiences — from concept to deployment.
-      </Label>
+      <main style={{display:"flex",flexDirection:"column",gap:20}}>
+        <Label>
+          Hello 👋
+          <br />
+          I'm Mohammed Mahi 🧑‍💻
+        </Label>
+        <Label>Full Stack Web Developer | Laravel & React Specialist</Label>
+        <Label>
+          I craft modern, responsive, and scalable web applications using
+          powerful tools like Laravel, React.js, and Tailwind CSS. I focus on
+          building clean UI, secure backend systems, and smooth user experiences
+          — from concept to deployment.
+        </Label>
+      </main>
+      <div className="icon-group">
+        <Github className="icon" size={20} />
+        <Twitter className="icon" size={20} />
+        <Instagram className="icon" size={20} />
+        <Linkedin className="icon" size={20} />
+      </div>
     </div>
   );
 };
